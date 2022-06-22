d5e3fbf20770eaaa75f788967d63a182b01343b2
UtilityAcc <96780372+UtilityAcc@users.noreply.github.com>
2022-06-22 16:26:22 +0300

    Update CarsController.java

--- src/main/java/web/controllers/CarsController.java
+++ src/main/java/web/controllers/CarsController.java
@@ -6,23 +6,23 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
-import web.service.CarserviceImlp;
+import web.service.CarserviceImpl;
 
 @Controller
 public class CarsController {
 
-    private CarserviceImlp carserviceImlp;
+    private CarserviceImpl carserviceimpl ;
     private Car header;
 
     @Autowired
-    public CarsController(CarserviceImlp carserviceImlp, Car header) {
-        this.carserviceImlp = carserviceImlp;
+    public CarsController(CarserviceImpl arserviceimpl, Car header) {
+        this.carserviceimpl = arserviceimpl;
         this.header = header;
     }
 
     @GetMapping(value = "/cars")
     public String printWelcome(@RequestParam(value = "count", required = false, defaultValue = "5") Integer count, ModelMap model) {
-        model.addAttribute("cars", carserviceImlp.getCars(count.intValue()));
+        model.addAttribute("cars", carserviceimpl.getCars(count.intValue()));
         model.addAttribute("header", header.header());
         return "cars";
     }
