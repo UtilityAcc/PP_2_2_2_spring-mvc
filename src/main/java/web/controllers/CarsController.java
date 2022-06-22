a7978c12ebd4f532256c26ab6d3a6e47a68bde95
UtilityAcc <96780372+UtilityAcc@users.noreply.github.com>
2022-06-22 16:09:28 +0300

    Update CarsController.java

--- src/main/java/web/controllers/CarsController.java
+++ src/main/java/web/controllers/CarsController.java
@@ -1,34 +1,31 @@
 package web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import web.service.CarserviceImlp;
 
 @Controller
 public class CarsController {
 
+    private CarserviceImlp carserviceImlp;
+    private Car header;
+
+    @Autowired
+    public CarsController(CarserviceImlp carserviceImlp, Car header) {
+        this.carserviceImlp = carserviceImlp;
+        this.header = header;
+    }
+
     @GetMapping(value = "/cars")
-    public String printWelcome(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
-        List<Car> cars = new ArrayList<>();
-        String header = new Car().header();
-        cars.add(new Car("Bugatti", "Veyron", 2020));
-        cars.add(new Car("Lamborgini", "Diablo", 2000));
-        cars.add(new Car("Porsche", "911", 1995));
-        cars.add(new Car("Doge", "Viper", 2005));
-        cars.add(new Car("Lada", "Kalina", 1995));
-        if (count != null && count <= 5) {
-            cars = cars.stream().limit(count).collect(Collectors
-                    .toCollection(ArrayList::new));
-        }
-        model.addAttribute("cars", cars);
-        model.addAttribute("header", header);
+    public String printWelcome(@RequestParam(value = "count", required = false, defaultValue = "5") Integer count, ModelMap model) {
+        model.addAttribute("cars", carserviceImlp.getCars(count.intValue()));
+        model.addAttribute("header", header.header());
         return "cars";
     }
 }
+
 
