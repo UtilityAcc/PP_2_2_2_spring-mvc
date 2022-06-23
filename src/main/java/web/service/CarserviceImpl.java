0d65b33ba2830ff814d84c0b240fb2f3a78b0bc7
UtilityAcc <96780372+UtilityAcc@users.noreply.github.com>
2022-06-23 15:38:27 +0300

    Update CarserviceImpl.java

--- src/main/java/web/service/CarserviceImpl.java
+++ src/main/java/web/service/CarserviceImpl.java
@@ -3,21 +3,20 @@ package web.service;
 import web.dao.CarDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import web.dao.CardaoImpl;
 import web.models.Car;
 
 import java.util.List;
 
 @Component
-public class CarserviceImpl implements CarDao {
-    private CardaoImpl cardaoImlp;
+public class CarserviceImpl implements CarService {
+    private CarDao cardao;
     @Autowired
-    public CarserviceImpl(CardaoImpl cardaoImlp) {
-        this.cardaoImlp = cardaoImlp;
+    public CarserviceImpl(CarDao cardao) {
+        this.cardao = cardao;
     }
     @Override
     public List<Car> getCars(int  count){
-        return cardaoImlp.getCars(count);
+        return cardao.getCars(count);
     }
 
 }
